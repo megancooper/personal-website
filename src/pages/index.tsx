971e4c36e5d3d7afec83e4443007548d8ec9c3ad
Megan Cooper <megancooper1000@gmail.com>
2022-03-06 22:56:53 -0600

    Next 12, Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,6 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-// type Props = {
-//   allPosts: string[]
-// };
-
-// {allPosts}: Props
-
 const Index = () => (
   <>
     <Head>
@@ -15,7 +9,7 @@ const Index = () => (
     </Head>
     <ul className="ml-6 lg:ml-8 list-none text-sm text-black max-w-lg tracking-wide leading-6">
       <li>
-        <Image alt="profile" src="/assets/profile.jpg" height={160} width={270} />
+        <Image alt="profile" src="/assets/profile.jpg" height={160} width={240} quality={100} />
       </li>
       <li>
         Hello! I&apos;m Megan, a software engineer based in Austin, TX.
@@ -24,10 +18,15 @@ const Index = () => (
       </li>
 
       <li>
-        Currently working on some
-        {' '}
-        <Link href="/projects">projects</Link>
-        .
+        Currently working on some projects:
+
+        <ul>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://www.melo-app.org">Melo</a>
+            {' '}
+            - A simple music player.
+          </li>
+        </ul>
       </li>
 
       <li>
@@ -40,14 +39,9 @@ const Index = () => (
       <li><a href="https://drive.google.com/file/d/1fRVHauEzUkNUbLVnBCMSecJZ-B1lIflD/view?usp=sharing" rel="noopener noreferrer" target="_blank">Resume</a></li>
 
       <li><a href="mailto: megancooper1000@gmail.com">megancooper1000@gmail.com</a></li>
+
     </ul>
   </>
 );
 
 export default Index;
-
-// export const getStaticProps = async () => ({
-//   props: {
-//     allPosts: [],
-//   },
-// });
